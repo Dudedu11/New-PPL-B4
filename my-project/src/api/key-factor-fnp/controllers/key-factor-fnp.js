dd23f24e70cba8bbcc244cef6748a20c7e97e647
Dudedu11 <muhamad.rafli.tif20@polban.ac.id.com>
2022-05-19 11:52:40 +0700

    dsafsdg

---
+++ my-project/src/api/key-factor-fnp/controllers/key-factor-fnp.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ *  key-factor-fnp controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::key-factor-fnp.key-factor-fnp');
---
+++ my-project/src/api/key-factor-fnp/routes/key-factor-fnp.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * key-factor-fnp router.
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::key-factor-fnp.key-factor-fnp');
