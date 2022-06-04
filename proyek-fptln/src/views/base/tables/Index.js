ee84983db4f06d2216605d7d8c8b05c2229cf946
yoganizar7 <yoga.nizar.tif20@polban.ac.id>
2022-06-04 22:08:05 +0700

    FE delete pages

--- proyek-fptln/src/views/base/tables/Index.js
+++ proyek-fptln/src/views/base/tables/Index.js
@@ -3,5 +3,5 @@ import axios from 'axios';
 const urlpenguji = 'localhost:1337/api/pengujis?populate=*';
 export const UrlPenguji = () => axios.get(urlpenguji);
 
-const urlpeserta = 'http://192.168.100.3:1337/api/pesertas?populate[0]=pegawai.jabatan&populate[1]=pegawai.grade&populate[2]=pegawai.jenjang';
+const urlpeserta = 'http://localhost:1337/api/awdawdwas';
 export const UrlPeserta = () => axios.get(urlpeserta)
