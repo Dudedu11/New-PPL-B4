ab15ce06a748c5b6c3b7b5340b4c638e9e2926bd
yoganizar7 <yoga.nizar.tif20@polban.ac.id>
2022-06-05 16:08:02 +0700

    Desain Model Deployment Diagram

---
+++ strapi-proyek/strapi-proyek/src/api/awdawdwa/controllers/awdawdwa.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ *  awdawdwa controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::awdawdwa.awdawdwa');
